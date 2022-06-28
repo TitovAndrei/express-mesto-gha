42ffca42c524648aeb74cf745edf46469e58242b
Andrey Titov <n3kto.1@yandex.ru>
2022-06-28 21:28:29 +0300

    hotfixes: исправил ошибки автотестов

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,5 @@
 const { checkToken } = require('../utils/jwt');
-const { ERROR_CODE_BAD_AUTH, ERROR_CODE_DEFAULT } = require('../utils/constants');
+const { ERROR_CODE_BAD_AUTH } = require('../utils/constants');
 const AuthError = require('../errors/AuthError');
 
 // eslint-disable-next-line consistent-return
@@ -14,10 +14,7 @@ const isAuthorizen = (req, res, next) => {
     req.user = payload;
     next();
   } catch (err) {
-    if (err.name === 'AuthError') {
-      return res.status(ERROR_CODE_BAD_AUTH).send({ message: 'Авторизуйтесь для доступа' });
-    }
-    return res.status(ERROR_CODE_DEFAULT).send({ message: 'На сервере произошла ошибка, авторизуйтесь для доступа' });
+    return res.status(ERROR_CODE_BAD_AUTH).send({ message: 'Авторизуйтесь для доступа' });
   }
 };
 
