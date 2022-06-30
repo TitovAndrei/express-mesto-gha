255cc1b82ccbea16a9410ca316fd5d294b01206b
Andrey Titov <n3kto.1@yandex.ru>
2022-06-30 22:44:30 +0300

    hotfixes: исправил ошибки автотестов

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ app.post(
     body: Joi.object().keys({
       email: Joi.string().required().email(),
       password: Joi.string().required(),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().custom(isUrlValid),
     }),
   }),
   createUser,
@@ -40,9 +43,6 @@ app.post(
     body: Joi.object().keys({
       email: Joi.string().required().email(),
       password: Joi.string().required(),
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().custom(isUrlValid),
     }),
   }),
   login,
